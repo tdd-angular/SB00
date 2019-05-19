19f00a26981e53c71a2318431aae82ce622e4957
Bela Vamosi <vamosibeci@gmail.com>
2019-05-19 19:13:28 +0200

    add feature module to demo lazy loading

---
+++ src/app/feature/feature.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DemoFormComponent } from './demo-form/demo-form.component';
+
+@NgModule({
+  declarations: [DemoFormComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FeatureModule { }
