52f0a3320eba472de45789f0e0a544ff5a2bd3de
Bela Vamosi <vamosibeci@gmail.com>
2019-05-19 19:41:38 +0200

    set up angular material with new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    HomeModule
+    HomeModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -1,11 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DemoFormComponent } from './demo-form/demo-form.component';
+import { MatAutocompleteModule, MatFormFieldModule, MatOptionModule } from '@angular/material';
+import { ObserversModule } from '@angular/cdk/observers';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { PlatformModule } from '@angular/cdk/platform';
+import { PortalModule } from '@angular/cdk/portal';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [DemoFormComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ObserversModule,
+    OverlayModule,
+    PlatformModule,
+    PortalModule,
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class FeatureModule { }
