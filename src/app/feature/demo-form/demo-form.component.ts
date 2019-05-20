82f353a3300c27f5c391d1ffa3ada59b238fdb7b
Bela Vamosi <vamosibeci@gmail.com>
2019-05-20 10:50:44 +0200

    add import of interface

--- src/app/feature/demo-form/demo-form.component.ts
+++ src/app/feature/demo-form/demo-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Airport } from '../entities/airport.entity';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
